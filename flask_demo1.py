960f5e1ae1554766e22738b6c7306abefa53aa69
Jiang Ya <jiangya89_@126.com>
2019-07-12 13:58:13 +0800

    flask初探

---
+++ flask_demo1.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/index")
+def index():
+    return 'hello it is my index page'
+
+if __name__ == "__main__":
+    app.run(debug=True)
